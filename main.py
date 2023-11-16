2344fd7017e55820191f138b3827f5a1b11ae676
CaioSommerOzorio <145836973+CaioSommerOzorio@users.noreply.github.com>
2023-11-17 07:44:11 +1100

    func names

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ cols, rows = 120, 36
 console = [[" "] * cols for _ in range(rows)]
 
 # Prints out img in any part of the console
-def add_img(img, x, y):
+def draw(img, x, y):
     for i in range(len(img)):
         for j in range(len(img[i])):
             if img[i][j] != "\n":
                 console[i+y][j+x] = img[i][j]
 
 # Same as add_img but adds text instead, singular loop because it's a string not a list of strings
-def add_text(text, x, y):
+def write(text, x, y):
     for i in range(len(text)):
         console[y][x+i] = text[i]
